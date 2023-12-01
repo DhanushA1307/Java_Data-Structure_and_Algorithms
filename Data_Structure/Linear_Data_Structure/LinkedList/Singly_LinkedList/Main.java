480ed61eb2005a90927dc4c71bb17f549df2aeb8
Dhanush A <dhanushanand1307@gmail.com>
2023-12-01 23:53:30 +0530

    Creating a Main file to accessing on the other files which contains related to singly linked list operations

---
+++ Data_Structure/Linear_Data_Structure/LinkedList/Singly_LinkedList/Main.java
@@ -0,0 +1,11 @@
+package Data_Structure.Linear_Data_Structure.LinkedList.Singly_LinkedList;
+import java.util.*;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        sc.close();
+    }
+}
