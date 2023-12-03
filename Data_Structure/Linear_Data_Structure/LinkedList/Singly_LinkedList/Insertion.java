663b6fd87f32eef634871fc78f11a7740dd56e1b
Dhanush A <dhanushanand1307@gmail.com>
2023-12-03 07:16:18 +0530

    Created a Insertion function for inserting in the beginning of the node

--- Data_Structure/Linear_Data_Structure/LinkedList/Singly_LinkedList/Insertion.java
+++ Data_Structure/Linear_Data_Structure/LinkedList/Singly_LinkedList/Insertion.java
@@ -3,5 +3,18 @@ import java.util.*;
 
 public class Node<T> insertion
 {
-    
+    public Node<T> insertAtBegin(Node<T> head, T data)
+    {
+        if(head == null)
+        {
+            head = new Node<T>(data);
+        }
+        else
+        {
+            Node<T> temp = new Node<T>(data);
+            temp.next = head;
+            head = temp;
+        }
+        return head;
+    }
 }
