42b1c2529412cafd108c5c0a7bd4aed0134fca9c
Dhanush A <dhanushanand1307@gmail.com>
2023-12-07 00:21:36 +0530

    Created an file called as "Others" to present in irrespective of Linked List progressive.

---
+++ Data_Structure/Linear_Data_Structure/LinkedList/Singly_LinkedList/Others.java
@@ -0,0 +1,9 @@
+package Data_Structure.Linear_Data_Structure.LinkedList.Singly_LinkedList;
+
+public class Others {
+    public void Prints(Node<T> head){
+        System.out.print("\t");
+        for(Node<T> temp = head; temp != null; temp = temp.next) System.out.print(temp.data+" ");
+        System.out.print("NULL\n");
+    }
+}
