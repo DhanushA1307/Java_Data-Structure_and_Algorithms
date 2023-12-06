d35f17511d66f8bdeecccbf6fb7e03f8a6274dc2
Dhanush A <72044811+DhanushA1307@users.noreply.github.com>
2023-12-06 09:24:00 +0530

    Create a Delete file for accessing to delete the node in first element

---
+++ Data_Structure/Linear_Data_Structure/LinkedList/Singly_LinkedList/Deletion.java
@@ -0,0 +1,9 @@
+package Data_Structure.Linear_Data_Structure.LinkedList.Singly_LinkedList;
+import java.util.*;
+
+public class Deletion
+{
+    public Node<T> delete_begin(Node<T> head){
+        return (head.next);
+    }
+}
