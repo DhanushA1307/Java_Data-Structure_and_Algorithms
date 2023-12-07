adc678eb7961323e906e2ed1beb1671aef287531
Dhanush A <dhanushanand1307@gmail.com>
2023-12-07 23:05:13 +0530

    To find the middle element of the node

--- Data_Structure/Linear_Data_Structure/LinkedList/Singly_LinkedList/Others.java
+++ Data_Structure/Linear_Data_Structure/LinkedList/Singly_LinkedList/Others.java
@@ -6,4 +6,9 @@ public class Others{
         for(Node<T> temp = head; temp != null; temp = temp.next) System.out.print(temp.data+" ");
         System.out.print("NULL\n");
     }
+    public Node<T> middle(Node<T> head){
+        Node<T> slow = head;
+        for(Node<T> fast = head; fast != null && fast.next != null; fast = fast.next.next) slow = slow.next;
+        return slow;
+    }
 }
