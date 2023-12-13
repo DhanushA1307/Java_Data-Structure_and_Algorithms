d5edd4715773dbbb043da1998e2a9d9ef19d7a0d
Dhanush A <dhanushanand1307@gmail.com>
2023-12-13 20:01:05 +0530

    Creating a main file for doing the functions of array -> inseriton, deletion, and other's related to arrays concept

---
+++ Data_Structure/Linear_Data_Structure/Arrays/Main.java
@@ -0,0 +1,11 @@
+package Data_Structure.Linear_Data_Structure.Arrays;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        sc.close(); 
+    }
+}
